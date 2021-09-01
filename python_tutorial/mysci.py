7c7da95b32a8168b8b7ba253e8b15c37e1e933b7
Dongjun Lee <dongjunl@cheyenne2.cheyenne.ucar.edu>
2021-09-01 14:34:37 -0600

    Ouput formatting comparison data

--- python_tutorial/mysci.py
+++ python_tutorial/mysci.py
@@ -1,8 +1,8 @@
 
 # Read the data file
-columns ={'date':0, 'time':1, 'tempout':2, 'windspeed':7}
+columns ={'date':0, 'time':1, 'tempout':2, 'windspeed':7, 'windchill':12}
 
-types = {'tempout': float, 'windspeed':float}
+types = {'tempout': float, 'windspeed':float, 'windchill':float}
 
 data = {}
 
@@ -48,4 +48,16 @@ windchill = []
 for temp, windspeed in zip(data['tempout'], data['windspeed']):
     windchill.append(compute_windchill(temp, windspeed))
 
-print(windchill)
+## print(windchill)
+# for wc_data, wc_comp in zip(data['windchill'], windchill):
+#     print(f'{wc_data:.5f} {wc_comp:.5f} {wc_data - wc_comp:.5f}')
+
+# Output comparison of data
+print('               ORIGINAL  COMPUTED')
+print(' DATE   TIME  WINDCHILL WINDCHILL DIFFERENCE')
+print('------- ----- --------- --------- ----------')
+zip_data = zip(data['date'], data['time'], data['windchill'], windchill)
+for date, time, wc_orig, wc_comp in zip_data:
+    wc_diff = wc_orig - wc_comp
+    print(f'{date} {time:>6} {wc_orig:9.6f} {wc_comp:9.6f} {wc_diff:10.6f}')
+
